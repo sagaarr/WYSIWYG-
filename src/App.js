cca6ced448e2454d16a35e21a0adf42a90f85734
sagar <sagar@carcility.com>
2022-02-22 04:11:36 +0530

    UI update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import {useEffect, useRef, useState} from 'react';
+import { useEffect, useRef, useState } from 'react';
 import ReactMarkdown from 'react-markdown'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AddLink from './common/modals/AddLink';
 
-const App = () =>  {
+const App = () => {
   const textAreaRef = useRef();
   const [areaText, setAreaText] = useState('');
   const [linkModal, setLinkModal] = useState(false);
@@ -15,15 +15,15 @@ const App = () =>  {
   const toggleLinkModal = () => setLinkModal(!linkModal);
 
   const textTransform = (value, updateFor) => {
-    if(textAreaRef.current !== undefined) {
+    if (textAreaRef.current !== undefined) {
       let start = textAreaRef.current.selectionStart;
       let end = textAreaRef.current.selectionEnd;
-      let textChange = areaText.slice(start,end);
+      let textChange = areaText.slice(start, end);
       let updateText;
-      if(updateFor == 'heading' || updateFor == 'blockquote') {
-         updateText = `${areaText.replace(textChange,`${value} ${areaText.substring(start,end)}`)}`;
-      }else {
-         updateText = `${areaText.replace(textChange,`${value}${areaText.substring(start,end)}${value}`)}`;
+      if (updateFor == 'heading' || updateFor == 'blockquote') {
+        updateText = `${areaText.replace(textChange, `${value}${" "}${areaText.substring(start, end)}`)}`;
+      } else {
+        updateText = `${areaText.replace(textChange, `${value}${areaText.substring(start, end).trim()}${value}`)}`;
       }
       setAreaText(updateText)
     };
@@ -31,42 +31,38 @@ const App = () =>  {
 
 
   const linkClick = () => {
-    if(textAreaRef.current !== undefined) {
+    if (textAreaRef.current !== undefined) {
       let start = textAreaRef.current.selectionStart;
       let end = textAreaRef.current.selectionEnd;
-      console.log(areaText.substring(start,end));
-      setSelectedText(areaText.substring(start,end));
+      console.log(areaText.substring(start, end));
+      setSelectedText(areaText.substring(start, end));
 
       toggleLinkModal();
-      }
-    };
-  
+    }
+  };
 
-    const handleLinkSubmit = ({link, linkText}) => {
-      if(textAreaRef.current !== undefined) {
-        let start = textAreaRef.current.selectionStart;
-        let end = textAreaRef.current.selectionEnd;
-        let textChange = areaText.slice(start,end);
-        let updateText = `${areaText.replace(textChange,`[${linkText}](${link})`)}`;;
-       
-        setAreaText(updateText)
-      };
-      toggleLinkModal();
+
+  const handleLinkSubmit = ({ link, linkText }) => {
+    if (textAreaRef.current !== undefined) {
+      let start = textAreaRef.current.selectionStart;
+      let end = textAreaRef.current.selectionEnd;
+      let textChange = areaText.slice(start, end);
+      let updateText = `${areaText.replace(textChange, `[${linkText}](${link})`)}`;;
+
+      setAreaText(updateText)
     };
+    toggleLinkModal();
+  };
 
 
   return (
     <div className="App">
-      <h4>Editor</h4>
-      1. Bold - DONE
-      2. Italic - DONE
-      3. Underline 
-      4. Heading/Sub-heading (at least 3 levels). - DONE
-      5. Block quote. - DONE
-      6. Convert to Caps case, uppercase and lowercase. 
-    <div className='outer-border'>
-      <div className='header'>
-        <button onClick={() => console.log('Hellloo Preview ')}>Preview</button>
+      
+      <h4 className='title'>Editor</h4>
+      <div className="row">
+        <div className="col-md-6">
+        <div className='header'>
+        <button onClick={() => console.log('Hellloo Preview ')} style={{marginLeft:'20px'}}>Preview</button>
         {/* Text transform into Bold Italic underline block-quote */}
         <button onClick={() => textTransform('**', 'B')}>B</button>
         <button onClick={() => textTransform('*', 'I')}>I</button>
@@ -77,33 +73,38 @@ const App = () =>  {
         <button onClick={linkClick}><i className="fas fa-link"></i></button>
         <button><i class="fas fa-list-ol"></i></button>
         <button><i className="fas fa-list"></i></button>
-        {/* Set Heading */}        
+        {/* Set Heading */}
         <select onChange={(v) => textTransform(v.target.value, 'heading')}>
-        <option value="">Heading</option>
+          <option value="">Heading</option>
           <option value="#">Heading 1</option>
           <option value="##">Heading 2</option>
           <option value="###">Heading 3</option>
         </select>
       </div>
-      <textarea 
-      name="" 
-      id="" 
-      cols="88" 
-      rows="20" 
-      ref={(elemet) => textAreaRef.current = elemet} 
-      value = {areaText}
-      onChange={(event)=>{
-        setAreaText(event.target.value);
-     }}
+ 
+      <textarea
+        name=""
+        id=""
+        cols="88"
+        rows="20"
+        ref={(elemet) => textAreaRef.current = elemet}
+        value={areaText}
+        onChange={(event) => {
+          setAreaText(event.target.value);
+        }}
       >
-        
-
       </textarea>
-    </div>
-    <div className='markdown-field'>
-    <ReactMarkdown>{areaText}</ReactMarkdown>
-    </div>
-    <AddLink show={linkModal} hide={toggleLinkModal} selectedLink={selectedText} handleLinkSubmit={handleLinkSubmit}/>
+        </div>
+        <div className="col-md-6">
+        <div className='markdown-field'>
+        <ReactMarkdown>{areaText}</ReactMarkdown>
+      </div>
+        </div>
+      </div>
+     
+
+    
+      <AddLink show={linkModal} hide={toggleLinkModal} selectedLink={selectedText} handleLinkSubmit={handleLinkSubmit} />
     </div>
   );
 }
